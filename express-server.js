48530dcb2d2b9e0242ee35c450df7a07f646678c
Nschulz88 <natalie.schulz@me.com>
2018-03-20 23:04:26 +0000

    created urls_new.ejs to be rendered when visiting urls/new / Created function to generate random String

--- express-server.js
+++ express-server.js
@@ -1,20 +1,36 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 // this sets the view engine to ejs
 app.set('view engine', "ejs");
 
-var urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  let randomString = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < 6; i++) {
+    randomString += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return randomString;
+}
+
+console.log(generateRandomString());
 
 // urls index page 
 app.get('/urls', (req, res) => {
     let templateVars = { urls: urlDatabase };
     res.render('pages/urls_index', templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render('pages/urls_new');
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -23,6 +39,11 @@ app.get("/urls/:id", (req, res) => {
     urls: urlDatabase
   };
   res.render('pages/urls_show', templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
