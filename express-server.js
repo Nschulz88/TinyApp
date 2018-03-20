6fbc6a6dcea27befa2a4526bdc71408b61ea2065
Nschulz88 <natalie.schulz@me.com>
2018-03-20 20:00:41 +0000

    Updated package.json and set up urls_show and urls_index -- WORKING APP

--- express-server.js
+++ express-server.js
@@ -2,13 +2,27 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+// this sets the view engine to ejs
+app.set('view engine', "ejs");
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.end("Hello!");
+
+// urls index page 
+app.get('/urls', (req, res) => {
+    let templateVars = { urls: urlDatabase };
+    res.render('pages/urls_index', templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { 
+    shortURL: req.params.id,
+    urls: urlDatabase
+  };
+  res.render('pages/urls_show', templateVars);
 });
 
 app.listen(PORT, () => {
